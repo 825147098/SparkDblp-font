5f3c78e31c723d5f64a3a9cb6e5e49feb2f962cb
Kirin <825147098@qq.com>
2020-06-25 09:34:05 +0800

    页面大概

---
+++ src/views/children/publiaction.vue
@@ -0,0 +1,325 @@
+<template>
+    <el-container>
+        <el-header height="60px">
+            <h3 class="headline">Search &nbsp; For &nbsp; Publications</h3>
+        </el-header>
+        <el-container>
+            <el-aside width="400px" style="padding: 20px">
+                <el-tabs type="border-card">
+                    <el-tab-pane :disabled=flag>
+                        <span slot="label">作者划分</span>
+                        <div style="font-size: 13px" v-for="item in autList" :key="item" class="divider">
+                            <el-button type="text" @click="searchAuthor(item)" size="small">
+                                {{item}}({{sortAuthor[item].length}})
+                            </el-button>
+                        </div>
+                    </el-tab-pane>
+                    <!--<el-tab-pane :disabled=flag>-->
+                        <!--<span slot="label">Veneu划分</span>-->
+                        <!--<div style="font-size: 13px" v-for="item in venList" :key="item">-->
+                            <!--{{item}}({{sortVen[item].length}})-->
+                        <!--</div>-->
+                    <!--</el-tab-pane>-->
+                    <!--<el-tab-pane :disabled=flag>-->
+                    <!--<span slot="label">类型划分</span>-->
+                    <!--</el-tab-pane>-->
+                    <el-tab-pane :disabled=flag>
+                        <span slot="label">年份划分</span>
+                        <div style="font-size: 13px" v-for="item in nowYear" :key="item">
+                            {{item}}({{sortData[item].length}})
+                        </div>
+                    </el-tab-pane>
+                </el-tabs>
+            </el-aside>
+            <el-main>
+                <header class="hide-head">
+                    <h2>Publication search results</h2>
+                </header>
+                <el-col>
+                    <ul class="pub-list" v-for="year in nowYear " :key="year">
+                        <li>{{year}}</li>
+                        <br>
+                        <li v-for="item in sortData[year]" :key="item"
+                            style="display: inline;padding: 20px; width: 80%;margin: auto">
+                            <el-link :href=item.ee[0]._VALUE style="padding-right: 20px" :underline="false">
+                                <el-button circle icon="el-icon-document" size="mini"></el-button>
+                            </el-link>
+                            <cite style="display: table-cell; font: inherit; padding: 0 2px; max-width: 800px">
+                    <span v-for="authors in item.author" :key="authors">
+                            {{authors._VALUE}}
+                            <el-tooltip class="item" effect="dark" :content=authors._orcid placement="bottom-end"
+                                        v-if="authors._orcid != null">
+                                <el-image src="https://dblp2.uni-trier.de/img/orcid-mark.12x12.png"
+                                          style="padding-left:0.25em;" alt=""></el-image>
+                            </el-tooltip>
+                        <span v-if="item.author.indexOf(authors) < item.author.length - 1">,</span>
+                        </span>
+                                :<br>
+                                <span>{{item.title}}.</span>
+                                <span v-for="school in item.school" :key="school">{{school}},</span>
+                                <!--类型划分加链接-->
+                                <span>{{item.publisher}}</span>
+                                <span v-for="it in item.year" :key="it">{{it}}</span>,
+                                <span v-if="item.isbn != null">ISBN &nbsp;</span>
+                                <span v-for="isbn in item.isbn" :key="isbn">{{isbn}},</span>pp.
+                                <span v-for="page in item.pages" :key="page">{{page}}</span>
+                            </cite><br>
+                        </li>
+                    </ul>
+                </el-col>
+            </el-main>
+        </el-container>
+    </el-container>
+</template>
+
+<script>
+    export default {
+        name: "publiaction",
+        data() {
+            return {
+                nowYear: [],
+                publishData: [
+                    {
+                        "_id": {"$oid": "5eedef19310f336f410ac92b"},
+                        "_key": "series/faia/2005-148",
+                        "_mdate": "2020-01-31",
+                        "author": [
+                            {
+                                "_VALUE": "Kristian Kersting"
+                            }
+                        ],
+                        "ee": [
+                            {
+                                "_VALUE": "http://www.booksonline.iospress.nl/Content/View.aspx?piid=96"
+                            },
+                            {
+                                "_VALUE": "http://d-nb.info/983957975"
+                            }
+                        ],
+                        "isbn": ["978-1-58603-674-4"],
+                        "pages": ["1-228"],
+                        "publisher": "IOS Press",
+                        "school": ["University of Freiburg, Germany"],
+                        "series": {
+                            "_VALUE": "Frontiers in Artificial Intelligence and Applications",
+                            "_href": "db/series/faia/index.html"
+                        },
+                        "title": "An Inductive Logic Programming Approach to Statistical Relational Learning",
+                        "volume": "148",
+                        "year": [2005]
+                    },
+                    {
+                        "_id": {"$oid": "5eedef19310f336f410ac92c"},
+                        "_key": "series/lnbip/Weber09",
+                        "_mdate": "2020-02-12",
+                        "author": [
+                            {
+                                "_VALUE": "Ingo M. Weber",
+                                "_orcid": "0000-0002-4833-5921"
+                            }
+                        ],
+                        "ee": [
+                            {
+                                "_VALUE": "https://doi.org/10.1007/978-3-642-05085-5"
+                            },
+                            {
+                                "_VALUE": "https://nbn-resolving.org/urn:nbn:de:1111-20091214234"
+                            },
+                            {
+                                "_VALUE": "http://d-nb.info/998030961"
+                            },
+                            {
+                                "_VALUE": "http://d-nb.info/999067885"
+                            },
+                            {
+                                "_VALUE": "https://www.wikidata.org/entity/Q58196259"
+                            }
+                        ],
+                        "isbn": ["978-3-642-05084-8", "978-3-642-05085-5"],
+                        "pages": ["3-231"],
+                        "publisher": "Springer",
+                        "school": ["Karlsruhe Institute of Technology, Germany"],
+                        "series": {
+                            "_VALUE": "Lecture Notes in Business Information Processing",
+                            "_href": "db/series/lnbip/index.html"
+                        },
+                        "title": "Semantic Methods for Execution-level Business Process Modeling - Modeling Support Through Process Verification and Service Composition",
+                        "volume": "40",
+                        "year": [2009]
+                    },
+                    {
+                        "_id": {"$oid": "5eedef19310f336f410ac92d"},
+                        "_key": "books/daglib/0068994",
+                        "_mdate": "2017-01-09",
+                        "author": [
+                            {
+                                "_VALUE": "Ulrich Junker"
+                            }
+                        ],
+                        "ee": [
+                            {
+                                "_VALUE": "http://d-nb.info/930724283"
+                            }
+                        ],
+                        "pages": ["I-VI, 1-187"],
+                        "school": ["Kaiserslautern University of Technology, Germany"],
+                        "title": "Relationships between assumptions.",
+                        "year": [1992]
+                    },
+                    {
+                        "_id": {"$oid": "5eedef19310f336f410ac92e"},
+                        "_key": "books/daglib/0069997",
+                        "_mdate": "2016-11-08",
+                        "author": [
+                            {
+                                "_VALUE": "Johannes Reichardt"
+                            }
+                        ],
+                        "ee": [
+                            {
+                                "_VALUE": "http://d-nb.info/860953998"
+                            }
+                        ],
+                        "pages": ["1-120"],
+                        "school": ["Darmstadt University of Technology, Germany"],
+                        "title": "Invarianz der Parsingeigenschaft unter Grammatikmorphismen.",
+                        "year": [1985]
+                    },
+                    {
+                        "_id": {"$oid": "5eedef19310f336f410ac92f"},
+                        "_key": "books/daglib/0073843",
+                        "_mdate": "2020-02-12",
+                        "author": [
+                            {
+                                "_VALUE": "Joachim Steinbach"
+                            }
+                        ],
+                        "ee": [
+                            {
+                                "_VALUE": "http://kbibmp3.ub.uni-kl.de/Preprint_Informatik/PS/dissertation_1.ps"
+                            },
+                            {
+                                "_VALUE": "https://nbn-resolving.org/urn:nbn:de:bsz:386-kluedo-712"
+                            },
+                            {
+                                "_VALUE": "http://d-nb.info/941908453"
+                            },
+                            {
+                                "_VALUE": "http://d-nb.info/956319327"
+                            }
+                        ],
+                        "pages": ["I-VIII, 1-288"],
+                        "school": ["Kaiserslautern University of Technology, Germany"],
+                        "title": "Termination of rewriting - extensions, comparison and automatic generation of simplification orderings.",
+                        "year": [1994]
+                    },
+                ],
+                sortData: [],
+                sortAuthor: [],
+                autList: [],
+                sortVen: [],
+                venList: [],
+                sortType: [],
+                typeList: [],
+
+            }
+        },
+
+        props:['text'],
+
+        methods: {
+            group_signal(data, key) {
+                return data.reduce(function (prev, cur) {
+                    (prev[cur[key]] = prev[cur[key]] || []).push(cur);
+                    return prev;
+                }, {});
+            },
+
+            groupBy() {
+                this.sortData = this.group_signal(this.publishData, "year");
+                console.log(this.sortData)
+            },
+
+            sortYear() {
+                let yearArr = [];
+                for (var i = 0; i < this.publishData.length; i++) {
+                    if (yearArr.indexOf((this.publishData[i].year[0])) == -1) {
+                        yearArr.push(this.publishData[i].year[0]);
+                    }
+                }
+                this.nowYear = yearArr.sort(function (a, b) {
+                    return b - a
+                });
+            },
+
+            groupByAuthor() {
+                let arr = []
+                for (let i = 0; i < this.publishData.length; i++) {
+                    for (let j = 0; j < this.publishData[i].author.length; j++) {
+                        arr.push({name: this.publishData[i].author[j]._VALUE, num: 0});
+                    }
+                }
+                this.sortAuthor = this.group_signal(arr, "name");
+
+                for (let i = 0; i < arr.length; i++) {
+                    if (this.autList.indexOf((arr[i].name)) == -1) {
+                        this.autList.push(arr[i].name);
+                    }
+                }
+            },
+
+            groupByVen() {
+                this.sortVen = this.group_signal(this.publishData, "journal");
+                let venArr = [];
+                for (var i = 0; i < this.publishData.length; i++) {
+                    if (venArr.indexOf((this.publishData[i].journal)) == -1) {
+                        venArr.push(this.publishData[i].journal);
+                    }
+                }
+                this.venList = venArr;
+            },
+
+            // groupByType() {
+            //     this.sortType = this.group_signal(this.publishData, "_publtype");
+            //     console.log(this.sortType);
+            // },
+            searchAuthor(authorName) {
+                console.log(authorName);
+                this.$emit("searchAuthor", authorName);
+            }
+        },
+        mounted() {
+            this.sortYear();
+            this.groupBy();
+            this.groupByAuthor();
+            // this.groupByType();
+            // this.groupByVen();
+        }
+    }
+</script>
+
+<style scoped>
+    .pub-list {
+        list-style-type: none;
+        display: block;
+        margin: 0;
+    }
+
+    .headline {
+        position: relative;
+        clear: both;
+        background: #606b70;
+        color: #ffffff;
+        padding: 4px 24px;
+        margin: 2ex 0em;
+    }
+    .hide-head{
+        border-bottom: 2px #7d848a solid;
+        min-height: 27px;
+        padding: 0px 16px 0px 24px;
+        margin: 4ex 0em 2ex 0em;
+    }
+    .divider{
+        border-bottom: 1px whitesmoke solid;
+    }
+</style>
---
+++ src/views/children/searchAut.vue
@@ -0,0 +1,67 @@
+<template>
+    <el-container>
+        <el-header height="60px">
+            <h3 class="headline">Search &nbsp; For &nbsp; Combined</h3>
+        </el-header>
+        <el-main>
+            <header class="hide-head">
+                <h2>Author search results</h2>
+            </header>
+            <ul class="pub-list">
+                <p>幸运匹配</p><br>
+                <el-button type="text" @click="searchAuthor"><li >sdfsdf</li></el-button>
+            </ul>
+            <ul class="pub-list">
+                <p>所有条匹配</p><br>
+                <li></li>
+            </ul>
+        </el-main>
+    </el-container>
+</template>
+
+<script>
+    export default {
+        name: "searchAut",
+
+        data(){
+            return{
+                authorName:"aedf",
+            }
+        },
+
+        methods:{
+
+            searchAuthor() {
+                this.$emit("searchAuthor", this.authorName);
+            }
+        },
+
+        mounted() {
+
+        }
+    }
+</script>
+
+<style scoped>
+    .headline {
+        position: relative;
+        clear: both;
+        background: #606b70;
+        color: #ffffff;
+        padding: 4px 24px;
+        margin: 2ex 0em;
+    }
+    .pub-list {
+        /*list-style-type: none;*/
+        display: block;
+        margin: 0;
+        font-size: 15px;
+    }
+
+    .hide-head{
+        border-bottom: 2px #7d848a solid;
+        min-height: 27px;
+        padding: 0px 16px 0px 24px;
+        margin: 4ex 0em 2ex 0em;
+    }
+</style>
